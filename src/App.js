9edbd43ada62c7f434c4b7447d5017ed5c897526
faiisll <faisal.ayas72@gmail.com>
2021-04-25 14:37:39 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, {useState } from "react";
+import styled, {ThemeProvider} from "styled-components";
+import {ResetCss} from './ResetCss';
+import {ligth, dark} from "./Styles";
+import {Header} from './components';
+import {
+  Switch,
+  Route,
+} from "react-router-dom";
+import {Home, DetailPage} from './containers';
+import {Context} from './Context';
 import './App.css';
 
+
+
 function App() {
+  const themeLocal = localStorage.getItem('theme')
+  const [themeMode, setThemeMode] = useState(themeLocal ? themeLocal: "light");
+  const [country, setCountry] = useState("all");
+  const value = { themeMode, setThemeMode, country, setCountry};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={themeMode === "dark" ? dark : ligth}>
+      <ResetCss />
+      <Context.Provider value={value}>
+
+      <Container>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/detail/:name">
+            <DetailPage />
+          </Route>
+        </Switch>
+
+      </Container>
+      </Context.Provider>
+    </ThemeProvider>
   );
 }
+
+const Container = styled.div`
+  background-color: ${props => props.theme.colors.bg};
+  width: 100%;
+  min-height: 100%;
+  position: absolute;
+`;
 
 export default App;
